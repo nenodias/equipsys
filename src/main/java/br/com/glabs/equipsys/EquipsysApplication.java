9b9b27e41e173c2abc1d7940118a305959e5d733
nenodias <horacio.dias92@gmail.com>
2022-01-06 13:56:30 -0300

    Commiting the initial project

---
+++ src/main/java/br/com/glabs/equipsys/EquipsysApplication.java
@@ -0,0 +1,13 @@
+package br.com.glabs.equipsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/glabs/equipsys/EquipsysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.glabs.equipsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
